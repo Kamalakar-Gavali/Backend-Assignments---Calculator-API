aeef0edc4e1f41d46ffaeb1370787cb4d2dc52fa
kamalakar <kg@gmail.com>
2020-12-02 18:25:19 +0530

    directly calc

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json())
 // your code goes here
 function validate(operation,num1,num2)
 {
-    if((num1 != parseInt(num1)||num2 !=parseInt(num2))&&(num1 != parseFloat(num1)||num2 !=parseFloat(num2)))
+    //if((num1 != parseInt(num1)||num2 !=parseInt(num2))&&(num1 != parseFloat(num1)||num2 !=parseFloat(num2)))
+    if(typeof(num1)=="string"||typeof(num2)=="string")
     {
         return ({status: "error",message:"Invalid data types"});
     }
@@ -29,23 +30,25 @@ function validate(operation,num1,num2)
     else{
         if(operation=='add')
         {
-           if(num1==parseInt(num1)&&num2==parseInt(num2))
+           /*if(num1==parseInt(num1)&&num2==parseInt(num2))
            {
             return({status: "success",message:"the sum of given two numbers",sum: parseInt(num1)+parseInt(num2)});
            }
            else{
             return({status: "success",message:"the sum of given two numbers",sum: parseFloat(num1)+parseFloat(num2)});
-           }
+           }*/
+           return({status: "success",message:"the sum of given two numbers",sum: (num1)+(num2)});
         }
         if(operation=='sub')
         {
-            if(num1==parseInt(num1)&&num2==parseInt(num2))
+            /*if(num1==parseInt(num1)&&num2==parseInt(num2))
             {
                 return({status: "success",message:"the difference of given two numbers",difference: parseInt(num1)-parseInt(num2)});    
             }
             else{
             return({status: "success",message:"the difference of given two numbers",difference: parseFloat(num1)-parseFloat(num2)});
-            }
+            }*/
+            return({status: "success",message:"the difference of given two numbers",difference:(num1)-(num2)});
         }
         if(operation=='div')
         {
@@ -54,24 +57,26 @@ function validate(operation,num1,num2)
             return({status: "error",message:"Cannot divide by zero"});
             }
             else{
-                if(num1==parseInt(num1)&&num2==parseInt(num2))
+                /*if(num1==parseInt(num1)&&num2==parseInt(num2))
                 {
                     return({status: "success",message:"The division of given numbers",result: parseInt(num1)/parseInt(num2)});
                 }
                 else{
                 return({status: "success",message:"The division of given numbers",result: parseFloat(num1)/parseFloat(num2)});
-                }
+                }*/
+                return({status: "success",message:"The division of given numbers",result:(num1)/(num2)});
             }
         }
         if(operation=='mul')
         {
-            if(num1==parseInt(num1)&&num2==parseInt(num2))
+            /*if(num1==parseInt(num1)&&num2==parseInt(num2))
             {
                 return({status: "success",message:"The product of given numbers",result: parseInt(num1)*parseInt(num2)});    
             }
             else{
             return({status: "success",message:"The product of given numbers",result: parseFloat(num1)*parseFloat(num2)});
-            }
+            }*/
+            return({status: "success",message:"The product of given numbers",result:(num1)*(num2)});
         }
         
     }
